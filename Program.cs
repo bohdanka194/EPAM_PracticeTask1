3669474af4c63c5d874a940c4ce4ce4017c25a1b
bohdanka194 <bohdanka194@gmail.com>
2017-03-15 12:08:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_.NET_PracticeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create vector1...");
+            Vector vector1 = new Vector(new int[] { 30,40,50,60,70,80});
+            Console.WriteLine("Output vector1: " + vector1.ToString());
+            Console.WriteLine("Element at index=3 in vector1: " + vector1[3]) ;
+            Console.WriteLine("Create vector2...");
+            Vector vector2 = new Vector(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 3, 8);
+            Console.WriteLine("Output vector2: " + vector2.ToString());
+            Console.WriteLine("Adding vector2 to vector1...");
+            vector1.AddVector(vector2);
+            Console.WriteLine("Output vector1: " + vector1.ToString());
+
+            Console.WriteLine("Create vector3...");
+            Vector vector3 = new Vector(new int[] {5,5,5,5,5,5 });
+            Console.WriteLine("Output vector3: " + vector3.ToString());
+            Console.WriteLine("Subtracting vector3 from vector1...  vector1 = vector1 - vector3");
+            vector1.SubtractVector(vector3);
+            Console.WriteLine("Output vector1: " + vector1.ToString());
+            
+            double coefficient = 5;
+            Console.WriteLine("Multipleing vector3 by coeff = " + coefficient);
+            vector3.MultiplyByCoefficient(coefficient);
+            Console.WriteLine("Output vector3: " + vector3.ToString());
+
+            Console.WriteLine("Compare vector1 and vector2... Is there equals? " + vector1.CompareWithVectors(vector2));
+           
+
+           
+            Console.Read();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_.NET_PracticeTask1
+{
+    class Vector
+    {
+        private int[] array;
+        private Vector(int size)
+        {
+            array = new int[size];
+        }
+        public Vector(IEnumerable<int> collection)
+        {
+            array = new int[collection.Count()];
+            for (int i = 0; i < collection.Count(); i++)
+            {
+                array[i] = collection.ElementAt(i);
+            }
+        }
+        public Vector(IEnumerable<int> collection, int startIndex, int finalIndex)
+        {
+            if (startIndex < 0 || 
+                startIndex > finalIndex ||
+                startIndex >= collection.Count() ||
+                finalIndex < 0 || 
+                finalIndex >= collection.Count())
+            {
+                throw new Exception("Exception with startIndex or finalIndex in constructor.");
+            }
+            array = new int[finalIndex - startIndex + 1];
+            for (int i = 0; i <= finalIndex-startIndex; i++)
+            {
+                array[i] = collection.ElementAt(i + startIndex);
+            }
+        } 
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= array.Count())
+                    throw new Exception("Index is out of range vector.");
+                return array[index]; 
+            }
+            set
+            {
+                if (index < 0|| index >= array.Count())
+                    throw new Exception("Index is out of range vector.");
+                array[index] = value; 
+            }
+        }
+        public Vector AddVector(Vector vectToAdd)
+        {
+            if (this.array.Count() != vectToAdd.array.Count())
+            {
+                throw new Exception("Adding is failed. Different size of vectors.");
+            }
+
+            for (int i = 0; i < this.array.Count(); i++)
+            {
+                this.array[i] = this.array[i] + vectToAdd.array[i];
+            }
+            return this;
+        }
+        
+        public Vector SubtractVector(Vector vectorToSubract)
+        {
+            if (this.array.Count() != vectorToSubract.array.Count())
+            {
+                throw new Exception("Subtracting is failed. Different size of vectors.");
+            }
+            
+            for (int i = 0; i < vectorToSubract.array.Count(); i++)
+            {
+                this.array[i] = this.array[i] - vectorToSubract.array[i];
+            }
+            return this;
+        }
+        public Vector MultiplyByCoefficient(double coefficient)
+        {          
+            for (int i = 0; i < this.array.Count(); i++)
+            {
+                this.array[i] = (int)(this.array[i] * coefficient);
+            }
+            return this;
+        }
+        public bool CompareWithVectors(Vector vectorToCompare)
+        {
+            bool isEqual = true;
+            if (this.Size() != vectorToCompare.Size())
+            {
+                isEqual = false;
+                return isEqual;
+            }
+            for (int i = 0; i < this.Size(); i++)
+            {
+                if (this[i] != vectorToCompare[i])
+                {
+                    isEqual = false;
+                }
+            }
+            return isEqual;
+        }
+        public int Size()
+        {
+            return array.Count();
+        }
+        public override string ToString()
+        {
+            String strVector = "";
+            for (int i = 0; i < this.Size(); i++)
+            {
+                strVector += this[i] + " ";
+            }
+            return strVector;
+        }
+    }
+}
